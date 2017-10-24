a10cf9f1113b28ae9dd041145bd9cd9e38a19fb3
Isaac Whitfield <iw@whitfin.io>
2017-10-24 14:01:44 -0700

    Remove unnecessary directory creation

--- src/exec.sh
+++ src/exec.sh
@@ -3,7 +3,6 @@
 # set a configuration file if not already set
 ! (: "${GEOIP_CONFIG_FILE?}") 2>/dev/null && {
     GEOIP_CONFIG_FILE="/usr/local/etc/GeoIP.conf"
-    mkdir -p $(dirname $GEOIP_CONFIG_FILE)
     echo "UserId ${GEOIP_USER_ID:-"999999"}" > $GEOIP_CONFIG_FILE
     echo "LicenseKey ${GEOIP_LICENSE_KEY:-"000000000000"}" >> $GEOIP_CONFIG_FILE
     echo "ProductIds ${GEOIP_PRODUCT_IDS:-"GeoLite2-City GeoLite2-Country"}" >> $GEOIP_CONFIG_FILE
