27e153077d94341212dd0944d56e55cc1bc9dd5b
Pavel Vaysberg <pavel@draft.com>
2018-03-26 18:11:31 -0400

    add ruby checker from ansible. change dockerfile to ruby alpine. add 'rolling' database replacement to support using a shared persistent volume

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine:latest
+FROM ruby:alpine
 
 ARG VERSION
 RUN apk add --update curl && \
@@ -24,5 +24,12 @@ RUN apk add --update curl && \
     rm -rf /tmp/geoipupdate* && \
     rm -rf /var/cache/apk/*
 
+ENV VALID_GEOIP_IP 185.244.215.243
+ENV GEOIP_USER_ID
+ENV GEOIP_PRODUCT_IDS GeoIP2-City
+ENV GEOIP_LICENSE_KEY
+ENV GEOIP_DIRECTORY /usr/local/share/GeoIP
+
+COPY validate_mmdb.rb /
 ADD src/exec.sh /
 CMD [ "/exec.sh" ]
--- src/exec.sh
+++ src/exec.sh
@@ -5,9 +5,15 @@
     GEOIP_CONFIG_FILE="/usr/local/etc/GeoIP.conf"
     echo "UserId ${GEOIP_USER_ID:-"999999"}" > $GEOIP_CONFIG_FILE
     echo "LicenseKey ${GEOIP_LICENSE_KEY:-"000000000000"}" >> $GEOIP_CONFIG_FILE
-    echo "ProductIds ${GEOIP_PRODUCT_IDS:-"GeoLite2-City GeoLite2-Country"}" >> $GEOIP_CONFIG_FILE
-    echo "DatabaseDirectory ${GEOIP_DIRECTORY:-"/usr/local/share/GeoIP"}" >> $GEOIP_CONFIG_FILE
+    echo "ProductIds ${GEOIP_PRODUCT_IDS:-"GeoLite2-City"}" >> $GEOIP_CONFIG_FILE
+    echo "DatabaseDirectory /tmp/}" >> $GEOIP_CONFIG_FILE
 }
 
 # execute the updates with verbose output
 geoipupdate -f $GEOIP_CONFIG_FILE -v
+
+ruby validate_mmdb.rb /tmp/GeoIP2-City.mmdb ${VALID_GEOIP_IP}
+
+[[ ! -d ${GEOIP_DIRECTORY} ]] && mkdir -p ${GEOIP_DIRECTORY}
+mv /tmp/GeoIP2-City.mmdb ${GEOIP_DIRECTORY}
+
