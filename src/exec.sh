55d40a98fb8b537058d1436630dbbe7ee637499d
Isaac Whitfield <iw@whitfin.io>
2018-08-15 10:02:03 -0700

    Update for latest geoipupdate changes

--- src/exec.sh
+++ src/exec.sh
@@ -3,8 +3,12 @@
 # set a configuration file if not already set
 ! (: "${GEOIP_CONFIG_FILE?}") 2>/dev/null && {
     GEOIP_CONFIG_FILE="/usr/local/etc/GeoIP.conf"
-    echo "UserId ${GEOIP_USER_ID:-"999999"}" > $GEOIP_CONFIG_FILE
-    echo "LicenseKey ${GEOIP_LICENSE_KEY:-"000000000000"}" >> $GEOIP_CONFIG_FILE
+    [[ ! -z $GEOIP_USER_ID ]] && {
+        echo "UserId $GEOIP_USER_ID" > $GEOIP_CONFIG_FILE
+    }
+    [[ ! -z $GEOIP_LICENSE_KEY ]] && {
+        echo "LicenseKey $GEOIP_LICENSE_KEY" > $GEOIP_CONFIG_FILE
+    }
     echo "ProductIds ${GEOIP_PRODUCT_IDS:-"GeoLite2-City GeoLite2-Country"}" >> $GEOIP_CONFIG_FILE
     echo "DatabaseDirectory ${GEOIP_DIRECTORY:-"/usr/local/share/GeoIP"}" >> $GEOIP_CONFIG_FILE
 }
